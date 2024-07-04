2d21aababb3565f709c199e8cd191ef512ffad21
Ben Franklin <225748@live.brock.ac.uk>
2024-07-04 22:39:38 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace EmailTestProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chkSSL = new System.Windows.Forms.CheckBox();
+            this.txtCC = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtSmtp = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtBcc = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(216, 76);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(454, 20);
+            this.txtTo.TabIndex = 0;
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(216, 141);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(454, 20);
+            this.txtSubject.TabIndex = 3;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.AcceptsReturn = true;
+            this.txtMessage.Location = new System.Drawing.Point(216, 165);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMessage.Size = new System.Drawing.Size(454, 162);
+            this.txtMessage.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(172, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "To:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Subject:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(157, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Message:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(595, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chkSSL
+            // 
+            this.chkSSL.AutoSize = true;
+            this.chkSSL.Enabled = false;
+            this.chkSSL.Location = new System.Drawing.Point(520, 349);
+            this.chkSSL.Name = "chkSSL";
+            this.chkSSL.Size = new System.Drawing.Size(49, 17);
+            this.chkSSL.TabIndex = 7;
+            this.chkSSL.Text = " SSL";
+            this.chkSSL.UseVisualStyleBackColor = true;
+            // 
+            // txtCC
+            // 
+            this.txtCC.Location = new System.Drawing.Point(216, 97);
+            this.txtCC.Name = "txtCC";
+            this.txtCC.Size = new System.Drawing.Size(454, 20);
+            this.txtCC.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(172, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "CC:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txtSmtp);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txtPort);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtUsername);
+            this.groupBox1.Enabled = false;
+            this.groupBox1.Location = new System.Drawing.Point(216, 338);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(298, 100);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(89, 69);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Smtp Server:";
+            // 
+            // txtSmtp
+            // 
+            this.txtSmtp.Location = new System.Drawing.Point(163, 66);
+            this.txtSmtp.Name = "txtSmtp";
+            this.txtSmtp.Size = new System.Drawing.Size(129, 20);
+            this.txtSmtp.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 69);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Port:";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(41, 66);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(42, 20);
+            this.txtPort.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(5, 43);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Password:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(69, 40);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(223, 20);
+            this.txtPassword.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Username:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(69, 19);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(223, 20);
+            this.txtUsername.TabIndex = 11;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(170, 122);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(29, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Bcc:";
+            // 
+            // txtBcc
+            // 
+            this.txtBcc.Location = new System.Drawing.Point(216, 119);
+            this.txtBcc.Name = "txtBcc";
+            this.txtBcc.Size = new System.Drawing.Size(454, 20);
+            this.txtBcc.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtBcc);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCC);
+            this.Controls.Add(this.chkSSL);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.txtTo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox chkSSL;
+        private System.Windows.Forms.TextBox txtCC;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtSmtp;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtBcc;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System.Windows.Forms;
+using System.Text;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+//uses gmail API instead of smtp as google got rid of "let less secure apps access" feature in favour of API 0-Auth2
+//requires Gmail API package is installed (do via tools and nuget)
+
+namespace EmailTestProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void sendEmail(string credentialsFileLocation, string addressTo, string body = "", string subject = "", string cc = "", string bcc = "")
+        {   
+            // variable = "" means that these parameters are optional, if nothing is passed for them, the default will be used (in this case the default is "")
+            // reason for the wierd ordering is so that optional parameters come up in the order of their probabilty of use
+
+            //see below for optional arguments
+            //https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/named-and-optional-arguments#:~:text=productName%7D%22)%3B%0A%20%20%20%20%7D%0A%7D-,Optional%20arguments,-The%20definition%20of
+            //or
+            //https://www.w3schools.com/cs/cs_method_parameters_default.phpS
+            //
+
+            string[] Scopes = { GmailService.Scope.GmailSend };
+            string ApplicationName = "SendMail";
+
+            string Base64UrlEncode(string input)
+            {
+                var data = Encoding.UTF8.GetBytes(input);
+                return Convert.ToBase64String(data).Replace("+", "-").Replace("/", "_").Replace("=", "");
+            }
+
+            UserCredential credential;
+            //read your credentials file
+            using (FileStream stream = new FileStream(credentialsFileLocation, FileMode.Open, FileAccess.Read))
+            {
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                path = Path.Combine(path, ".credentials/gmail-dotnet-quickstart.json");
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(GoogleClientSecrets.Load(stream).Secrets, Scopes, "user", CancellationToken.None, new FileDataStore(path, true)).Result;
+            }
+            //compile the email
+            string Message = $"To:{addressTo}" +
+                $"\r\nCC:{cc}" +
+                $"\r\nBcc:{bcc}" +
+                $"\r\nSubject:{subject}" +
+                $"\r\nContent-Type: text/html;charset=utf-8\r\n";// +
+                //$"\r\n<body>{text}</body>";
+
+            var sr = new StringReader(body);
+            List<string> listOfLines = new List<string>();
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {// rn is for the formatting of the message <br> is line breaks
+                if (line == "")
+                {
+                    Message = Message + "\r\n" + "<br>"; //if we need to skip an empty line, just add another break
+                }
+                else
+                {
+                    Message = Message + "\r\n" + "<br>" + line; //go to a new line before adding this one
+                }
+            }
+
+
+            //call your gmail service, giving your credentials
+            var service = new GmailService(new BaseClientService.Initializer() { HttpClientInitializer = credential, ApplicationName = ApplicationName });
+            var msg = new Google.Apis.Gmail.v1.Data.Message();
+            msg.Raw = Base64UrlEncode(Message.ToString());
+            try
+            {
+                service.Users.Messages.Send(msg, "me").Execute();
+                MessageBox.Show("Your email has been successfully sent !", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("There was an error in sending your email", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            string credentialsFileLocation = "C:/Users/benja/source/repos/EmailTestProject/bin/Debug/credentials.json";
+            string addressTo = txtTo.Text;
+            string cc = txtCC.Text;
+            string bcc = txtBcc.Text;
+            string subject = txtSubject.Text;
+            string body = txtMessage.Text;
+
+            //for info on how to send parameters out of order see this
+            //https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/named-and-optional-arguments#:~:text=If%20you%20don%27t%20remember%20the%20order%20of%20the%20parameters%20but%20know%20their%20names%2C%20you%20can%20send%20the%20arguments%20in%20any%20order.
+            sendEmail(credentialsFileLocation: credentialsFileLocation, addressTo: addressTo, body: body, subject: subject, cc: cc, bcc: bcc);
+
+            //for this form the above is best as we dont mind sending empty text anyways but if the computer was writing the email
+            //the below options may serve better
+
+            //equally, you don't need to send all/any of the optional parametrs
+            //sendEmail(credentialsFileLocation: credentialsFileLocation, addressTo: addressTo);
+
+            //or you can send the parameters out of order so long as you specifiy what parameters you are sending
+            //(credentialsFileLocation: credentialsFileLocation, addressTo: addressTo, body: body, subject: subject, cc: cc, bcc: bcc);
+
+            //or you can just send them normally, in order
+            //sendEmail(credentialsFileLocation, addressTo, body, subject, cc, bcc);
+
+        }
+
+    }
+}
